beff942519e6781c2355ed509f112d99e4a2c8f0
Paul Applegate <paul@theapplegates.com>
2022-09-06 14:02:22 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import picture from '@astrojs/image';
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://micro.paulapplegate.com',
+  integrations: [mdx(), sitemap(), image()]
+});
